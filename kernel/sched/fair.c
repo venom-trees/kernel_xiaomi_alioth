df505dd4f5d7529a8e869608603bcbd2f7a3c6b6
Rick Yiu <rickyiu@google.com>
2019-03-13 15:48:03 +0800

    BACKPORT: sched/fair: if sync flag ignored, try to place in mid cluster
    If the sync flag is ignored because the current cpu is not in
    the affinity mask for the target of a sync wakeup (usually
    binder call), prefer to place in the mid cluster if
    possible. The main case is a "top-app" task waking a
    "foreground" task when the top-app task is running on
    a CPU that is not in the foreground cpuset. This patch
    causes the search order from mid capacity cpu be used
    when the sync flag failed.
    
    backport from commit 98ae57d9eaf7
    ("ANDROID: sched/fair: if sync flag ignored, try to place in same cluster")
    
    Bug: 117438867
    Bug: 144961676
    Test: boot to home, operation normal
    Change-Id: I68d0cc05db1bc2cb02d4445c71b02215209e8c04
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: Jimmy Shiu <jimmyshiu@google.com>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7976,16 +7976,13 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 	int delta = 0;
 	int task_boost = per_task_boost(p);
 	int boosted = (schedtune_task_boost(p) > 0) || (task_boost > 0);
-	int start_cpu = get_start_cpu(p);
+	int start_cpu = get_start_cpu(p, sync_boost);
 
-<<<<<<< HEAD
 	if (is_many_wakeup(sibling_count_hint) && prev_cpu != cpu &&
 			cpumask_test_cpu(prev_cpu, &p->cpus_allowed))
 		return prev_cpu;
 
 	start_cpu = get_start_cpu(p, sync_boost);
-=======
->>>>>>> 6a0a9ca35815 (Revert "sched/walt: Improve the scheduler")
 	if (start_cpu < 0)
 		goto eas_not_ready;
 
